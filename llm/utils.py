900aa9d0f8ee10498dde12c5f38b6c3c5d81d0bd
Milutin-S <studen.m98@gmail.com>
2024-04-01 00:00:48 +0200

    Add messages creation

--- llm/utils.py
+++ llm/utils.py
@@ -1,9 +1,17 @@
 import os
 from pathlib import Path
+from typing import Dict, List
 
 import yaml
 from openai import OpenAI
 from openai.types.chat import ChatCompletion
+
+from llm.prompts import (
+    CONTEXT_PROMPT,
+    CONVERSATION_PROMPT,
+    QUERY_PROMPT,
+    SYSTEM_PROMPT,
+)
 
 config_path = Path("./config.yaml")
 with config_path.open("r") as file:
@@ -22,3 +30,15 @@ def get_answer(
     )
 
     return response
+
+
+def get_messages(context: str, query: str, conversation: List[str]) -> List[Dict]:
+    return [
+        {"role": "system", "content": SYSTEM_PROMPT},
+        {
+            "role": "user",
+            "content": CONVERSATION_PROMPT.format(conversation=conversation),
+        },
+        {"role": "user", "content": CONTEXT_PROMPT.format(context=context)},
+        {"role": "user", "content": QUERY_PROMPT.format(query=query)},
+    ]
