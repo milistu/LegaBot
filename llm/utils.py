fdbdd19f7a0f64f620bffd308b7e242e15375a03
Milutin-S <studen.m98@gmail.com>
2024-04-28 23:53:50 +0200

    Convert OpenAI client to argument.

--- llm/utils.py
+++ llm/utils.py
@@ -17,13 +17,13 @@ config_path = Path("./config.yaml")
 with config_path.open("r") as file:
     config = yaml.safe_load(file)
 
-client = OpenAI(
-    api_key=os.environ["OPENAI_API_KEY"],
-)
+# client = OpenAI(
+#     api_key=os.environ["OPENAI_API_KEY"],
+# )
 
 
 def get_answer(
-    model: str, temperature: float, messages: list, stream: bool = False
+    client: OpenAI, model: str, temperature: float, messages: list, stream: bool = False
 ) -> ChatCompletion:
     response = client.chat.completions.create(
         model=model, temperature=temperature, messages=messages, stream=stream
