1c677917dbe5344f309738a9604b25c83f15df10
Milutin-S <studen.m98@gmail.com>
2024-05-27 18:25:34 +0200

    Add main method for calling the pipeline and arguments.

--- database/vector_database.py
+++ database/vector_database.py
@@ -1,9 +1,19 @@
 import argparse
 import json
+import os
 from pathlib import Path
 
 from loguru import logger
-from .utils import create_embeddings
+from qdrant_client import QdrantClient
+from tqdm.auto import tqdm
+
+from database.utils import (
+    create_collection,
+    create_embeddings,
+    get_count,
+    load_and_process_embeddings,
+    upsert,
+)
 
 
 def main(args: argparse.Namespace) -> None:
@@ -14,7 +24,29 @@ def main(args: argparse.Namespace) -> None:
         embeddings_dir=args.embeddings_dir,
         model=args.model,
     )
+
     logger.info("Creating vector database.")
+    qdrant_client = QdrantClient(
+        url=os.environ["QDRANT_CLUSTER_URL"],
+        api_key=os.environ["QDRANT_API_KEY"],
+    )
+    data_paths = list(args.embeddings_dir.iterdir())
+    for path in tqdm(data_paths, total=len(data_paths), desc="Creating collections"):
+        # Check if this is necessary
+        collection_name = path.stem.replace("-", "_")
+        collection_name = collection_name + "_TESTIC"
+        points = load_and_process_embeddings(path=path)
+
+        create_collection(client=qdrant_client, name=collection_name)
+        upsert(client=qdrant_client, collection=collection_name, points=points)
+
+        point_num = get_count(client=qdrant_client, collection=collection_name)
+        if not point_num == len(points):
+            logger.error(f"There are missing points in {collection_name} collection.")
+
+        logger.info(
+            f'Created "{collection_name}" collection with {point_num} data points.'
+        )
 
 
 if __name__ == "__main__":
@@ -22,11 +54,19 @@ if __name__ == "__main__":
         description="Create embeddings and vector database for scraped files."
     )
     parser.add_argument(
-        "scraped_dir", type=Path, help="Directory to the scraped files."
+        "--scraped_dir", type=Path, help="Directory to the scraped files."
     )
-    parser.add_argument("to_process_dir", type=Path, help="Directory to process files.")
     parser.add_argument(
-        "embeddings_dir", type=Path, help="Directory for storing embeddings."
+        "--to_process_dir",
+        type=Path,
+        default=Path("./database/to_process"),
+        help="Directory to process files.",
+    )
+    parser.add_argument(
+        "--embeddings_dir",
+        type=Path,
+        default=Path("./database/embeddings"),
+        help="Directory for storing embeddings.",
     )
     parser.add_argument(
         "--model",
