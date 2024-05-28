aaf9b626b124da49519601aed8845f6c03deaf96
Milutin-S <studen.m98@gmail.com>
2024-05-28 18:03:35 +0200

    Remove unused code, add docstring and comments.

--- router/query_router.py
+++ router/query_router.py
@@ -1,24 +1,8 @@
 import json
-from typing import Dict, List
+from typing import List
 
-import numpy as np
 from langfuse.decorators import observe
 from openai import OpenAI
-
-
-def rout_query(centroids: Dict[str, List], query_embedding: List) -> str:
-    centroids = list(centroids.items())
-    centroids_np = np.array([value for key, value in centroids])
-    query_np = np.array(query_embedding)
-
-    norm_query = np.linalg.norm(query_np)
-    norm_centroids = np.linalg.norm(centroids_np, axis=1)
-
-    cosine_similarities = np.dot(centroids_np, query_np) / (norm_centroids * norm_query)
-    max_index = np.argmax(cosine_similarities)
-
-    collection = centroids[max_index][0]
-    return collection
 
 
 @observe()
@@ -29,11 +13,26 @@ def semantic_query_router(
     temperature: float,
     model: str = "gpt-3.5-turbo",
 ) -> List[str]:
+    """
+    Routes a semantic query to the appropriate collections using OpenAI's API.
+
+    Args:
+        client (OpenAI): The OpenAI client instance.
+        query (str): The query string to be routed.
+        prompt (str): The prompt template to be used for the query.
+        temperature (float): The temperature setting for the model's response.
+        model (str, optional): The model to be used. Defaults to "gpt-3.5-turbo".
+
+    Returns:
+        List[str]: A list of collections that are relevant to the query.
+    """
+    # Create the completion request to the OpenAI API
     response = client.chat.completions.create(
         model=model,
         response_format={"type": "json_object"},
         messages=[{"role": "system", "content": prompt.format(query=query)}],
         temperature=temperature,
     )
+    # Parse the response to extract the collections
     collections = json.loads(response.choices[0].message.content)["response"]
     return collections
