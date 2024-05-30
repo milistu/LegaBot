e3df28c090df925e7e9a215f17243164b3816567
Milutin-S <studen.m98@gmail.com>
2024-05-30 22:16:09 +0200

    Remove unused code, add docstring and comments.

--- llm/utils.py
+++ llm/utils.py
@@ -1,7 +1,5 @@
-from pathlib import Path
 from typing import Dict, List
 
-import yaml
 from langfuse.decorators import observe
 from openai import OpenAI
 from openai.types.chat import ChatCompletion
@@ -13,23 +11,45 @@ from llm.prompts import (
     SYSTEM_PROMPT,
 )
 
-config_path = Path("./config.yaml")
-with config_path.open("r") as file:
-    config = yaml.safe_load(file)
-
 
 @observe()
 def get_answer(
-    client: OpenAI, model: str, temperature: float, messages: list, stream: bool = False
+    client: OpenAI,
+    model: str,
+    temperature: float,
+    messages: List[Dict],
+    stream: bool = False,
 ) -> ChatCompletion:
-    response = client.chat.completions.create(
+    """
+    Get an answer from the OpenAI chat model.
+
+    Args:
+        client (OpenAI): The OpenAI client instance.
+        model (str): The model name to use.
+        temperature (float): The temperature setting for the model.
+        messages (List[Dict]): The list of messages to send to the model.
+        stream (bool, optional): Whether to stream the response. Defaults to False.
+
+    Returns:
+        ChatCompletion: The chat completion response from OpenAI.
+    """
+    return client.chat.completions.create(
         model=model, temperature=temperature, messages=messages, stream=stream
     )
 
-    return response
-
 
 def get_messages(context: str, query: str, conversation: List[str]) -> List[Dict]:
+    """
+    Prepare the list of messages for the chat model.
+
+    Args:
+        context (str): The context information.
+        query (str): The user's query.
+        conversation (List[str]): The conversation history.
+
+    Returns:
+        List[Dict]: The list of messages formatted for the chat model.
+    """
     return [
         {"role": "system", "content": SYSTEM_PROMPT},
         {
