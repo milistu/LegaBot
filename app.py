82ce07720510533d6379d49938ec0c94dd7dcca2
Milutin Studen <81291567+milistu@users.noreply.github.com>
2024-06-01 23:59:58 +0200

    Chat UI (#50)
    * Update app, add sidebar, relovace methods to utils
    
    * Update poetry files.
    
    * Add const variable and move methods from app.
    
    * Made minimal requirements for deployment.
    
    * Add comments.
    
    * Change method names.

--- app.py
+++ app.py
@@ -1,119 +1,73 @@
-import json
-import os
-from pathlib import Path
-
 import streamlit as st
-import yaml
 from dotenv import find_dotenv, load_dotenv
-from langfuse.decorators import observe
-from loguru import logger
-from openai import OpenAI
-from qdrant_client import QdrantClient
 
-from database.utils import embed_text, get_context, search
-from llm.prompts import DEFAULT_CONTEXT, INTRODUCTION_MESSAGE
-from llm.utils import get_answer, get_messages
-from router.query_router import semantic_query_router
-from router.router_prompt import DEFAULT_ROUTER_RESPONSE, ROUTER_PROMPT
-
-load_dotenv(find_dotenv())
-
-st.title("Legal ChatBot")
-
-qdrant_client = QdrantClient(
-    url=os.environ["QDRANT_CLUSTER_URL"],
-    api_key=os.environ["QDRANT_API_KEY"],
+from llm.prompts import INTRODUCTION_MESSAGE
+from utils import (
+    generate_response,
+    initialize_clients,
+    load_config,
+    WARNING_MESSAGE,
+    QUERY_SUGGESTIONS,
+    AUTHORS,
+    LOGO_URL,
 )
 
-openai_client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
-
-config_path = Path("./config.yaml")
-centroid_path = Path("./router/collection_centroids.json")
-
-with config_path.open("r") as file:
-    config = yaml.safe_load(file)
-
-# Delete this
-with open(centroid_path, "r", encoding="utf-8") as file:
-    centroids = json.loads(file.read())
+# Load environment variables from the .env file.
+load_dotenv(find_dotenv())
 
 
-@observe()
-def response_generator(query: str):
-    st.session_state.messages = st.session_state.messages[
-        -1 * config["openai"]["gpt_model"]["max_conversation"] :
-    ]
+# Set Streamlit page configuration with custom title and icon.
+st.set_page_config(page_title="Your Lawyer Assistant", page_icon=LOGO_URL)
+st.title("LegaBot")
+st.divider()
 
-    # st.session_state.messages.append({"role": "user", "content": query})
+# Initialize API clients for OpenAI and Qdrant and load configuration settings.
+openai_client, qdrant_client = initialize_clients()
+config = load_config()
 
-    embedding_response = embed_text(
-        client=openai_client,
-        text=query,
-        model=config["openai"]["embedding_model"]["name"],
-    )
-    embedding = embedding_response.data[0].embedding
+# Display the logo and set up the sidebar with useful information and links.
+st.logo(LOGO_URL, icon_image=LOGO_URL)
+with st.sidebar:
+    st.subheader("💡 Query Suggestions")
+    with st.container(border=True, height=200):
+        st.markdown(QUERY_SUGGESTIONS)
 
-    # Rout query
-    collections = semantic_query_router(
-        client=openai_client,
-        model=config["openai"]["gpt_model"]["router"],
-        query=query,
-        prompt=ROUTER_PROMPT,
-        temperature=config["openai"]["gpt_model"]["temperature"],
-    )
-    logger.info(f"Query routed to collections: {collections}")
+    st.subheader("⚠️ Warning")
+    with st.container(border=True):
+        st.markdown(WARNING_MESSAGE)
 
-    if collections[0] == DEFAULT_ROUTER_RESPONSE:
-        context = DEFAULT_CONTEXT
-    else:
-        search_results = []
-        for collection_name in collections:
-            search_results.extend(
-                search(
-                    client=qdrant_client,
-                    collection=collection_name,
-                    query_vector=embedding,
-                    limit=10,
-                    with_vectors=True,
-                )
-            )
-
-        top_k = 15 if len(collections) > 1 else 10
-        context = get_context(search_results=search_results, top_k=top_k)
-
-    stream = get_answer(
-        client=openai_client,
-        model=config["openai"]["gpt_model"]["llm"],
-        temperature=config["openai"]["gpt_model"]["temperature"],
-        messages=get_messages(
-            context=context, query=query, conversation=st.session_state.messages
-        ),
-        stream=True,
-    )
-
-    for chunk in stream:
-        part = chunk.choices[0].delta.content
-        if part is not None:
-            yield part
+    st.subheader("✍️ Authors")
+    st.markdown(AUTHORS)
 
 
+# Initialize or update the session state for storing chat messages.
 if "messages" not in st.session_state:
     st.session_state.messages = [{"role": "assistant", "content": INTRODUCTION_MESSAGE}]
 
+# Display all chat messages stored in the session state.
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-
+# Handle user input and generate responses.
 if prompt := st.chat_input("Postavi pitanje iz prava..."):
-    # Generate and display the response
+    # Append user message to session state.
     st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
+
+    # Display user message in chat container.
     with st.chat_message("user"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        stream = response_generator(prompt)
+        # Generate a response using the LLM and display it as a stream.
+        stream = generate_response(
+            query=prompt,
+            openai_client=openai_client,
+            qdrant_client=qdrant_client,
+            config=config,
+        )
+        # Write the response stream to the chat.
         response = st.write_stream(stream)
 
+    # Append assistant's response to session state.
     st.session_state.messages.append({"role": "assistant", "content": response})
