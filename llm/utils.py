2340840bb774bc1df0d95d09d7c79703b36701c9
Milutin-S <studen.m98@gmail.com>
2024-06-03 18:55:35 +0200

    Remove GPT call method.

--- llm/utils.py
+++ llm/utils.py
@@ -1,9 +1,5 @@
 from typing import Dict, List
 
-from langfuse.decorators import observe
-from openai import OpenAI
-from openai.types.chat import ChatCompletion
-
 from llm.prompts import (
     CONTEXT_PROMPT,
     CONVERSATION_PROMPT,
@@ -12,33 +8,9 @@ from llm.prompts import (
 )
 
 
-@observe()
-def get_answer(
-    client: OpenAI,
-    model: str,
-    temperature: float,
-    messages: List[Dict],
-    stream: bool = False,
-) -> ChatCompletion:
-    """
-    Get an answer from the OpenAI chat model.
-
-    Args:
-        client (OpenAI): The OpenAI client instance.
-        model (str): The model name to use.
-        temperature (float): The temperature setting for the model.
-        messages (List[Dict]): The list of messages to send to the model.
-        stream (bool, optional): Whether to stream the response. Defaults to False.
-
-    Returns:
-        ChatCompletion: The chat completion response from OpenAI.
-    """
-    return client.chat.completions.create(
-        model=model, temperature=temperature, messages=messages, stream=stream
-    )
-
-
-def get_messages(context: str, query: str, conversation: List[str]) -> List[Dict]:
+def formate_messages_chat(
+    context: str, query: str, conversation: List[str]
+) -> List[Dict]:
     """
     Prepare the list of messages for the chat model.
 
